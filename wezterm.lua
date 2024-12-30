ec3b9f31723f68e0fe572e78298b84d8fdbf49aa
macpro-6-1 <macpro-6-1@macpro-6-1s-Mac-Pro.local>
2024-12-30 01:22:47 -0500

    Update Gradient

--- wezterm.lua
+++ wezterm.lua
@@ -31,7 +31,7 @@ config.window_padding = {
 -- Background settings
 -- Add gradient background
 config.window_background_gradient = {
-  colors = { '#0d0d1a', '#1a1a2e' },
+  colors = { '#000000', '#0a0a1f', '#1a1a33' },
   orientation = { Linear = { angle = -45.0 } },
 }
 
